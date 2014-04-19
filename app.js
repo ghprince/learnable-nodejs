f37b41347cfe9332dd127508a8371cf1478a5737
Gordon Gao <ghprince@gmail.com>
2014-04-19 17:22:13 +0800

    Extract mappings into a module

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 
 var http = require('http');
 
-var mappings = {
-  g: 'http://www.google.com'
-};
+var mappings = require('./mappings');
 
 var server = http.createServer(function (req, res) {
   var alias = req.url.substring(1);
---
+++ mappings.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var mappings = {
+  g: 'http://www.google.com'
+};
+
+module.exports = mappings;
