af9af0aadaaaa426039bf6b4809d00e7c9ce8ea4
Gordon Gao <ghprince@gmail.com>
2014-04-19 17:30:51 +0800

    modified the module to provide api instead of whole array

--- app.js
+++ app.js
@@ -5,16 +5,14 @@ var http = require('http');
 var mappings = require('./mappings');
 
 var server = http.createServer(function (req, res) {
-  var alias = req.url.substring(1);
+  var mapping = mappings.get(req.url);
 
-  if (!mappings[alias]) {
+  if (!mapping) {
     res.writeHead(404);
     return res.end();
   }
 
-  res.writeHead(302, {
-    location: mappings[alias]
-  });
+  res.writeHead(302, { location: mapping });
   res.end();
 });
 
--- mappings.js
+++ mappings.js
@@ -1,7 +1,20 @@
 'use strict';
 
+var data = {
+  g: 'http://www.google.com',
+  a: 'http://www.amazon.com'
+};
+
 var mappings = {
-  g: 'http://www.google.com'
+  get: function (url) {
+    var alias = url.substring(1);
+
+    if (!data[alias]) {
+      return undefined;
+    }
+
+    return data[alias];
+  }
 };
 
 module.exports = mappings;
